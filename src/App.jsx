37cd2679106d2e009f581c12e4c1a474ce0f7486
Julyane Oliveira <tech.julyoliv@gmail.com>
2024-12-04 00:59:22 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Footer from "./components/footer/Footer.jsx";
+import Header from "./components/Header/header.jsx";
+import "./GlobalStyle/globalStyle.scss";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
