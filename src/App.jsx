3d7136365c6342e9b19d29030988c9a38a5ade0b
Julyane Oliveira <tech.julyoliv@gmail.com>
2024-12-04 01:11:30 -0300

    fix import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Footer from "./components/footer/Footer.jsx";
 import Header from "./components/header/Header.jsx";
-import "./GlobalStyle/globalStyle.scss";
+import "./globalStyle/globalStyle.scss";
 
 export default function App() {
   return (
